67dff0d5f4987049928d84b07985c2670fb22277
Italo "Finiz" Souza <bhz.italo@gmail.com>
2026-03-26 08:35:24 -0300

    remove arquivos desnecessarios (.vscode e session)

--- mysql-local.session.sql
+++
@@ -1,9 +0,0 @@
-USE sistema_academico;
-
-SELECT 
-    aluno.nome AS aluno,
-    curso.nome AS curso,
-    matricula.data_matricula
-FROM matricula
-JOIN aluno ON matricula.aluno_id = aluno.id
-JOIN curso ON matricula.curso_id = curso.id;
